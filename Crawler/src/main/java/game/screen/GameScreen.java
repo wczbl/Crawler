9d847ad425bae429f6ddb9878f1889e28a8022ed
wczbl <130032524+wczbl@users.noreply.github.com>
2024-06-04 11:08:30 +0200

    Add files via upload

--- Crawler/src/main/java/game/screen/GameScreen.java
+++ Crawler/src/main/java/game/screen/GameScreen.java
@@ -3,6 +3,9 @@ package game.screen;
 import game.GameComponent;
 import game.InputHandler;
 import game.entity.*;
+import game.entity.mob.Bat;
+import game.entity.mob.Slime;
+import game.entity.mob.Zombie;
 import game.gfx.*;
 import game.level.Level;
 import game.math.MathUtil;
@@ -20,6 +23,8 @@ public class GameScreen extends Screen {
     private Player player = new Player(Vec3.zero);
     private Camera cam;
     private ViewPort viewPort;
+    private static long[] estimates = new long[120];
+    private static int estimateIndex;
 
     // UI Shenanigans
     private int lowHealthCol = 0x7F0000;
@@ -28,28 +33,32 @@ public class GameScreen extends Screen {
     private int lowTimeCol = 0xFF6A00;
     private int highTimeCol = 0xFFD800;
 
+    private int enemyCount;
+
     public GameScreen() {
         cam = new Camera(player);
         viewPort = new ViewPort(GAME_VIEW_WIDTH, GAME_VIEW_HEIGHT);
         level = new Level(32, 32);
         level.add(player);
 
-        for (int i = 0; i < level.w * level.h; i++) {
-            if (Math.random() < 0.02) level.add(new Slime(new Vec3(i % level.w, 0, i / level.w)));
-            if (Math.random() < 0.05) level.add(new Campfire(new Vec3(i % level.w, 0, i / level.w)));
-            if (Math.random() < 0.1) level.add(new Tree(new Vec3(i % level.w, 0, i / level.w)));
-            if (Math.random() < 0.3) level.add(new BigTree(new Vec3(i % level.w, 0, i / level.w)));
+        level.add(new Slime(new Vec3(1, 0, 1)));
+        level.add(new Bat(new Vec3(1, 0, 2)));
+        level.add(new Zombie(new Vec3(2, 0, 1)));
+        level.add(new Slime(new Vec3(2, 0, 2)));
 
-        }
     }
 
     public void tick(InputHandler input) {
         cam.tick();
         level.tick();
         player.tickInput(input);
+
+        if (level.tickTime % estimates.length == 0) System.out.println("Render average: " + getAverageEstimate() + " ms");
     }
 
     public void render(Bitmap screen) {
+        long last = System.currentTimeMillis();
+
         viewPort.updateZBuffer();
         level.render(viewPort, cam);
         viewPort.renderFloor(cam, level);
@@ -84,6 +93,18 @@ public class GameScreen extends Screen {
         // ICON DRAWING
         screen.draw(Art.gui[0], 3, 162);
         screen.draw(Art.gui[1], 220, 161);
+
+        long now = System.currentTimeMillis();
+        estimates[estimateIndex++ % estimates.length] = now - last;
+    }
+
+    public static double getAverageEstimate() {
+        long result = 0;
+        for (long estimate : estimates) {
+            result += estimate;
+        }
+
+        return result / (double) estimates.length;
     }
 
 }
